ad5b8712ed539fb83150b3cce0997ec76dca4789
Dain-123 <daintyteresadavis9@gmail.com>
2025-06-13 23:08:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,6 +15,6 @@ def predict():
     final = [np.array(features)]
     prediction = model.predict(final)[0]
     return render_template('index.html', prediction_text=f'Predicted Price: ${prediction:.2f}') 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  
-    app.run(host='0.0.0.0', port=port)        
+if _name_ == '_main_':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
