7e896b272d4fdf9a7439c7bbdaf3fa32ae389b84
Dain-123 <daintyteresadavis9@gmail.com>
2025-06-13 21:18:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import os
 import joblib
 import numpy as np
 
@@ -17,4 +18,5 @@ def predict():
     return render_template('index.html', prediction_text=f'Predicted Price: ${{prediction:.2f}}')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+port = int(os.environ.get("PORT", 5000))
+app.run(host='0.0.0.0', port=port)  
