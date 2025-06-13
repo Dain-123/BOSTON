b9942027d5e39417943d4b8814f739beb760c9af
Dain-123 <daintyteresadavis9@gmail.com>
2025-06-13 21:20:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ import joblib
 import numpy as np
 
 app = Flask(__name__)
-model = joblib.load('model.pkl')
-
+model = joblib.load('model.pkl')  
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -15,8 +14,7 @@ def predict():
     features = [float(x) for x in request.form.values()]
     final = [np.array(features)]
     prediction = model.predict(final)[0]
-    return render_template('index.html', prediction_text=f'Predicted Price: ${{prediction:.2f}}')
-
+    return render_template('index.html', prediction_text=f'Predicted Price: ${prediction:.2f}') 
 if __name__ == "__main__":
-port = int(os.environ.get("PORT", 5000))
-app.run(host='0.0.0.0', port=port)  
+    port = int(os.environ.get("PORT", 5000))  
+    app.run(host='0.0.0.0', port=port)        
